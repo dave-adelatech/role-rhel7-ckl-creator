f3677856b9b86ec3661da9563157fc86fa94a22d
Andrew Darley <andrew.darley.ctr@navy.mil>
2019-08-28 16:53:49 -0400

    v2r4 Quarterly DISA updates

--- files/V-71849.sh
+++ files/V-71849.sh
@@ -11,23 +11,22 @@ OS=`uname`
 OSver=`uname -r`
 ##if [ X${OS} = XSunOS ] || [ X${OSver} != X5.10 ];then exit 1;fi
 #
-#
+# save rpm output for files that have been modified to a file
 >$HN.${VULN}
-rpm -Va 2>/dev/null | grep '^.M' >$HN.$VULN
+rpm -Va 2>/dev/null |egrep -i '^\.[M|U|G|.]{8}' >$HN.$VULN
 
 # Loop through each file found
 allbad=""
+# cut out just the file name
 for fl in `cut -c14- $HN.$VULN|sort|uniq`
 do
 bad=""
-#get the package
 pkg=`rpm -qf $fl`
 perm1=`stat -c %a $fl`
-perm2=`rpm -ql $pkg --dump |grep "^$fl " |cut -d" " -f5|cut -c5-`
-
-if [ `echo $perm2 |wc -c` -eq 3 ];
-then perm2="0${perm2}"
-fi
+perm2=`rpm -ql $pkg --dump |grep "^$fl " |cut -d" " -f5`
+if [ `echo $perm2 |wc -c` -eq 7 ]; then perm2="0${perm2}";fi
+if [ `echo $perm2 |wc -c` -eq 3 ]; then perm2="0${perm2}";fi
+perm2=`echo $perm2 |cut -c5-`
 
 #User
 aa=`echo $perm1 |cut -c1`
@@ -63,7 +62,8 @@ fi
 #Working on ownership/group
 #rpm -ql McAfeeVSEForLinux-2.0.3.29216-29216.x86_64 --dump |grep "^/opt/NAI/package/McAfeeVSEForLinux "|awk '{print $6$7}'
 aa=`rpm -ql $pkg --dump |grep "^$fl " |awk '{print $6$7}'`
-bb=`ls -ld /opt/NAI/package/McAfeeVSEForLinux|awk '{print $3$4}'`
+#bb=`ls -ld /opt/NAI/package/McAfeeVSEForLinux|awk '{print $3$4}'`
+bb=`ls -ld $fl |awk '{print $3$4}'`
 if [ X$aa != X$bb ]
 then bad=fail
 	allbad=fail
@@ -83,4 +83,3 @@ fi
 #echo "$FINDING $VULN $STIG $TITLE" #>>$SC_NAM
 
 rm $HN.${VULN}
-
